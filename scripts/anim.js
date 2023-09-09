5650ff5a3f7221020831c8cc2e48aedb21e97ef6
Marvin Eversley Silva <meverss@gmail.com>
2023-09-09 17:44:09 -0400

    first commit

---
+++ scripts/anim.js
@@ -0,0 +1,39 @@
+const notes = document.querySelectorAll('.cuadro');
+const video = document.querySelector('#pool_screening')
+
+function triggerAnimation(entries){
+  entries.forEach(entry =>{
+    if(entry.isIntersecting){
+        entry.target.classList.add('animate__animated', 'animate__bounceIn');
+        // obBoxes.unobserve(entry.target);
+    }else{
+        entry.target.classList.remove('animate__animated', 'animate__bounceIn');
+    }
+  })  
+}
+
+function playVideo(entries){
+    if (entries[0].isIntersecting){
+        video.classList.add('animate__animated', 'animate__bounceIn');
+        video.play();
+        obVideo.unobserve(video);
+    } else {
+        video.classList.remove('animate__animated', 'animate__bounceIn');
+        video.pause();
+    }
+}
+
+const options={
+    //root: null,
+    // rootMargin: '-50px',
+    threshold: .7
+}
+
+const obBoxes = new IntersectionObserver(triggerAnimation, options);
+const obVideo = new IntersectionObserver(playVideo, options);
+
+notes.forEach(note =>{
+    obBoxes.observe(note);
+})
+
+obVideo.observe(video);
