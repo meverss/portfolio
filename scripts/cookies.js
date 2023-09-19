851b5c1547a36740de33a2bc30d90483d06f34ff
Marvin Eversley Silva <meverss@gmail.com>
2023-09-19 18:06:18 -0400

    Update

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,29 +1,29 @@
-function show_cookies_policy(){
-    page = location.origin +'/pages/cookies.html';
-    window.open(page, '', 'resizable=no, toolbar=0, status=0, top=200, left=500, width=800, height=600');
-}
+// function show_cookies_policy(){
+//     page = "http://localhost:5500/pages/cookies.html";
+//     window.open(page, '', 'resizable=no, toolbar=0, status=0, top=200, left=500, width=800, height=600');
+// }
 
-PDCookieConsent.config({
-    "defaultLang" : "en",
-    "brand": {
-        "dev" : false,
-        "name": "My Portfolio",
-        "url" : location.origin,
-        "websiteOwner" : "KiniunTech"
-    },
+// PDCookieConsent.config({
+//     "defaultLang" : "en",
+//     "brand": {
+//         "dev" : false,
+//         "name": "My Portfolio",
+//         "url" : "http://localhost:5500",
+//         "websiteOwner" : "KiniunTech"
+//     },
 
-    "cookiePolicyLink": location.origin + "/cookies.html",
-    "hideModalIn": [location.origin + "/cookies.html"],
+//     "cookiePolicyLink": "http://localhost:5500/pages/cookies.html",
+//     "hideModalIn": ["http://localhost:5500/pages/cookies.html"]
     
-    "styles": {
-        "primaryButton": {
-            "bgColor" : "#D2691E",
-            "txtColor": "#FFFFFF"
-        },
-    "secondaryButton": {
-        "bgColor" : "#CCC",
-        "txtColor": "#333333"
-    }
-    }
-});
+//     "styles": {
+//         "primaryButton": {
+//             "bgColor" : "#D2691E",
+//             "txtColor": "#FFFFFF"
+//         },
+//     "secondaryButton": {
+//         "bgColor" : "#CCC",
+//         "txtColor": "#333333"
+//     }
+//     }
+// });
 
