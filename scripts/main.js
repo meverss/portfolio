e90aeed1695544d6f748814e98031f97f86cafa5
Marvin Eversley Silva <meverss@gmail.com>
2023-10-03 05:03:32 -0400

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -233,10 +233,22 @@ frm_contact.addEventListener('submit', e => {
 
 });
 
-// CONTACT FORM SUBMIT CONFIG
+// CONFIG CONTACT FORM SUBMIT
 
 frm_key.value = "6812b923-1859-4cd0-a7b2-7f246e481715";
 frm_url.value = location.origin + '/portfolio';
+
+
+
+
+
+
+
+
+
+
+
+// =================================================
 
 // GET A JSON FILE FROM A URL TO GENERATE PREJECTS
 
